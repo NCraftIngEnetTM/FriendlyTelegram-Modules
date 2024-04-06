da1e48002349c856ad84b78bfcc253d364136baa
NCraftIngEnetTM <83570350+NCraftIngEnetTM@users.noreply.github.com>
2024-04-06 19:43:03 +0500

    Update NNcraftLogger.py

--- NNcraftLogger.py
+++ NNcraftLogger.py
@@ -1,11 +1,11 @@
 import asyncio
-import logging
+#import logging
 import json
 
 from telethon import types
 from .. import loader, utils
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
 @loader.tds
 class NNcraftLoggerMod(loader.Module):
@@ -35,7 +35,7 @@ class NNcraftLoggerMod(loader.Module):
             entity = await self.client.get_entity(id)
             try:
                 name = entity.first_name
-            except:
+            except AttributeError:
                 name = entity.title
             await self.client.send_message(int(forward[args[0]]["id"]), f"{name}:{id} добавлен в логгер!")
         
@@ -56,10 +56,50 @@ class NNcraftLoggerMod(loader.Module):
         entity = await self.client.get_entity(id)
         try:
             name = entity.first_name
-        except:
+        except AttributeError:
             name = entity.title
         await self.client.send_message(int(forward["id"]), f"{name}:{id} удален из логгера!")
+        
+    async def logmecmd(self, message):
+        await message.delete()
+        id = utils.get_chat_id(message)
 
+        ids = self._db.get(self.strings["db_name"], "ids")
+        settings = ids.get(str(id))
+        
+        if settings:
+            if not settings["me"]:
+                forward = self._db.get(self.strings["db_name"], "forwards")[settings["forward"]]
+                ids[str(id)]["me"] = True
+                self._db.set(self.strings["db_name"], "ids", ids)
+
+                entity = await self.client.get_entity(id)
+                try:
+                    name = entity.first_name
+                except AttributeError:
+                    name = entity.title
+                await self.client.send_message(int(forward["id"]), f"Логирование меня добавлено у {name}:{id}!")
+
+    async def unlogmecmd(self, message):
+        await message.delete()
+        id = utils.get_chat_id(message)
+
+        ids = self._db.get(self.strings["db_name"], "ids")
+        settings = ids.get(str(id))
+        
+        if settings:
+            if settings["me"]:
+                forward = self._db.get(self.strings["db_name"], "forwards")[settings["forward"]]
+                ids[str(id)]["me"] = False
+                self._db.set(self.strings["db_name"], "ids", ids)
+
+                entity = await self.client.get_entity(id)
+                try:
+                    name = entity.first_name
+                except AttributeError:
+                    name = entity.title
+                await self.client.send_message(int(forward["id"]), f"Логирование меня удалено у {name}:{id}!")
+            
     async def addforwardcmd(self, message):
         args = list(utils.get_args_split_by(message, " "))
         forwards = self._db.get(self.strings["db_name"], "forwards")
@@ -84,15 +124,34 @@ class NNcraftLoggerMod(loader.Module):
 
     async def listidscmd(self, message):
         ids = self._db.get(self.strings["db_name"], "ids")
-        await message.edit(json.dumps(ids))
+        text = "-"*20 + "\n"
+        for id in ids.keys():
+            try:
+                entity = await self.client.get_entity(int(id))
+                try:
+                    name = entity.first_name
+                except AttributeError:
+                    name = entity.title
+            except ValueError:
+                name = "Python-Entity-None"
+            text += f"{name}:{id}\n"
+            text += f" -- forward: {ids[id]['forward']}\n"
+            text += f" -- me: {ids[id]['me']}\n"
+            text += "-"*20 + "\n"
+        await message.edit(text)
 
     async def listforwardscmd(self, message):
         forwards = self._db.get(self.strings["db_name"], "forwards")
-        await message.edit(json.dumps(forwards))
+        text = "-"*20 + "\n"
+        for name in forwards.keys():
+            text += f"{name}\n"
+            text += f" — id: {forwards[name]['id']}\n"
+            text += "-"*20 + "\n"
+        await message.edit(text)
 
     async def watcher(self, message):
         id = utils.get_chat_id(message)
-        if not isinstance(message, types.Message):
+        if not isinstance(message, types.Message): 
             return
         try:
             settings = self._db.get(self.strings["db_name"], "ids")[str(id)]
